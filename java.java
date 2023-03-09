7935adb35a40a8ad1a957a0d58b65a11eb222664
Adarsh Kumar <126956545+chadarsh07@users.noreply.github.com>
2023-03-09 23:04:31 +0530

    Create java.java

---
+++ java.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class java{
+    public static void main(String args[]){
+System.out.println("enter two number");
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+System.out.println("addition"+(a+b));
+    }
+}
